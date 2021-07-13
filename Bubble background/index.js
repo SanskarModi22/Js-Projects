bc9f01c2cf0f8202070aa3f70acacbbbc9667c60
Bharat Kumar <kumarbharat661@gmail.com>
2021-07-13 11:50:13 +0530

    Corrected animation of bubble page

--- Bubble background/index.js
+++ Bubble background/index.js
@@ -1,4 +1,14 @@
-
+for(var i=1;i<=25;i++){
+    var bubbleid="#bub"+i+"";
+    var item=document.querySelector(bubbleid);
+    var size=""+Math.round(Math.random()*150+1)+"px";
+    console.log(size);
+    item.style.width=size;
+    item.style.height=size;
+    item.style.top=""+Math.random()*100+"vh";
+    item.style.left=""+Math.random()*100+"vw";
+}
+function resize() {
     for(var i=1;i<=25;i++){
         var bubbleid="#bub"+i+"";
         var item=document.querySelector(bubbleid);
@@ -9,3 +19,5 @@
         item.style.top=""+Math.random()*100+"vh";
         item.style.left=""+Math.random()*100+"vw";
     }
+}
+setInterval(resize,8000);
