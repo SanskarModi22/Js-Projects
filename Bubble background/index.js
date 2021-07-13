ffd285e4e6dd941aac28259340f641ad53a51432
Bharat Kumar <kumarbharat661@gmail.com>
2021-07-13 11:36:03 +0530

    adding Bubble background

---
+++ Bubble background/index.js
@@ -0,0 +1,11 @@
+
+    for(var i=1;i<=25;i++){
+        var bubbleid="#bub"+i+"";
+        var item=document.querySelector(bubbleid);
+        var size=""+Math.round(Math.random()*150+1)+"px";
+        console.log(size);
+        item.style.width=size;
+        item.style.height=size;
+        item.style.top=""+Math.random()*100+"vh";
+        item.style.left=""+Math.random()*100+"vw";
+    }
