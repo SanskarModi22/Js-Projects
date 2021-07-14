0f7bd42b695500b8282e353c91fc6c6f19280978
Bharat Kumar <kumarbharat661@gmail.com>
2021-07-14 10:14:44 +0530

    bubble background animation timing and hide on click added

--- Bubble background/index.js
+++ Bubble background/index.js
@@ -8,6 +8,16 @@ for(var i=1;i<=25;i++){
     item.style.top=""+Math.random()*100+"vh";
     item.style.left=""+Math.random()*100+"vw";
 }
+for(var i=1;i<=25;i++){
+    var bubbleid="#secBub"+i+"";
+    var item=document.querySelector(bubbleid);
+    var size=""+Math.round(Math.random()*150+1)+"px";
+    console.log(size);
+    item.style.width=size;
+    item.style.height=size;
+    item.style.top=""+Math.random()*100+"vh";
+    item.style.left=""+Math.random()*100+"vw";
+}
 function resize() {
     for(var i=1;i<=25;i++){
         var bubbleid="#bub"+i+"";
@@ -18,6 +28,29 @@ function resize() {
         item.style.height=size;
         item.style.top=""+Math.random()*100+"vh";
         item.style.left=""+Math.random()*100+"vw";
+        item.style.display = "block";
     }
 }
-setInterval(resize,8000);
+function resize2() {
+    for(var i=1;i<=25;i++){
+        var secBubbleid="#secBub"+i+"";
+        var item=document.querySelector(secBubbleid);
+        var size=""+Math.round(Math.random()*150+1)+"px";
+        console.log(size);
+        item.style.width=size;
+        item.style.height=size;
+        item.style.top=""+Math.random()*100+"vh";
+        item.style.left=""+Math.random()*100+"vw";
+        item.style.display = "block";
+    }
+}
+setInterval(resize,12000);
+setInterval(resize2,24000);
+
+var bubbles=document.querySelectorAll(".bub");
+
+for(var i=1;i<51;i++){
+    bubbles[i].addEventListener("click",function(){
+        this.style.display = "none";
+    });
+}
